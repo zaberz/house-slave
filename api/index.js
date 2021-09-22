ccf30a378cada33e3fa99df5fb1a86ce7ff7528b
zaber <z274588107@gmail.com>
2021-09-22 11:42:54 +0800

    init init

--- api/index.js
+++ api/index.js
@@ -1,18 +1,19 @@
 /**
-* vercel function entry
-* */
-require('dotenv').config()
+ * vercel function entry
+ * */
+require("dotenv").config();
 
-const express = require('express')
-const db = require('./model/index')
-const router = require('./router/index')
-let app = express()
-app.get('/', (req, res)=> {
+const express = require("express");
+const db = require("./model/index");
+const router = require("./router/index");
+let app = express();
+app.get("/", (req, res) => {
   res.json({
-    a:1
-  })
-})
+    a: 1
+  });
+});
 
-app.use('/api/crawl', router)
+console.log(1);
+app.use("/api/crawl", router);
 
-app.listen(9000)
+app.listen(9000);
